71f3f20b6f6ff60ed3635c924a20a06f83012869
poul1986 <you@example.com>
2020-06-03 17:22:52 +0300

    Saved solution in one file

---
+++ app.js
@@ -0,0 +1,51 @@
+﻿
+var slider = document.getElementById("myRange");
+var output = document.getElementById("currentPrice");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
+
+
+var app = angular.module('HotelApp', []);
+
+
+
+app.controller("HotelController", function ($scope, $http) {
+
+    var url = "data.json";
+
+    //window.setInterval(function () {
+
+    //}, 5000);
+    FetchData();
+
+    function FetchData() {
+        $http.get(url)
+            .then(function (response) {
+
+
+                $scope.Hotels = response.data[1].entries;
+
+                var prices = [];
+                for (var i in $scope.Hotels) {
+                    prices.push($scope.Hotels[i].price);
+                }
+                $scope.max = Math.max(...prices);
+                $scope.rangemax = $scope.max;
+
+
+            }, function myError(response) {
+                console.log(response);
+            });
+    }
+
+
+    $scope.filterPrice = function (obj) {
+        return obj.price > 0 && obj.price <= $scope.max;
+    };
+});
+
+
+
